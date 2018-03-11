22439e5f428e81286575b00daf5698c56857ff6a
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-03-10 20:20:33 -0800

    pass full name of tutors in get_tutors

--- schedule_lessons/signup/views.py
+++ schedule_lessons/signup/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate, login
+from .forms import MyRegistrationForm
 
 # Create your views here.
 
 def signup(request):
     if request.method == 'GET':
-        form = UserCreationForm()
+        form = MyRegistrationForm()
         return render(request, 'signup/signup.html', {'form': form})
     return HttpResponse(status=404)
 
@@ -16,11 +16,11 @@ def signup(request):
 # Create new Tutor
 def signup_tutor(request):
     if request.method == 'GET':
-        form = UserCreationForm()
+        form = MyRegistrationForm()
         return render(request, 'signup/signup.html', {'form': form, 'user_type': 'tutor'})
         
     elif request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyRegistrationForm(request.POST)
         print ('tutor')
         print (request.POST)
         if form.is_valid():
@@ -44,11 +44,11 @@ def signup_tutor(request):
 # Create new Client
 def signup_client(request):
     if request.method == 'GET':
-        form = UserCreationForm()
+        form = MyRegistrationForm()
         return render(request, 'signup/signup.html', {'form': form, 'user_type': 'client'})
         
     elif request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyRegistrationForm(request.POST)
         print('client')
         print (request.POST)
         if form.is_valid():
