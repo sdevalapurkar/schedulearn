63f9088d7d9865fae245422eb7a8189c52f35a59
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-03-10 18:21:46 -0800

    implement get_tutors view

--- schedule_lessons/scheduler/admin.py
+++ schedule_lessons/scheduler/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from .models import Relationships, Events
 
 # Register your models here.
 
-#from .models import Question
+admin.site.register(Events)
 
-#admin.site.register(Question)
+admin.site.register(Relationships)
