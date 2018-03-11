3b63d5f4636df6ab5eedc4f7be93c0315050e934
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-03-10 20:20:33 -0800

    pass full name of tutors in get_tutors

--- schedule_lessons/scheduler/views.py
+++ schedule_lessons/scheduler/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Relationships
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -24,8 +25,19 @@ def add_tutor(request):
 def get_tutors(request):
     if request.method == 'GET':
         tutors = []
+        print (request.user)
         clients_tutors = Relationships.objects.filter(client=request.user)
+
+        lee = User.objects.filter(username='leezeitz')
+        for user in lee:
+            print (user.first_name)
+
+        print('\n')
+
         for tutor in clients_tutors:
+            print(tutor.tutor.profile.id)
+            print(tutor.tutor)
+            print('full name: ' + tutor.tutor.get_full_name())
             tutors.append([tutor.tutor.first_name, tutor.tutor.last_name, tutor.tutor.profile.id])
 
         
