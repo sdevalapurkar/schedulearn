6ee53b7102d3a3f8c2997878e05ecd90e1890518
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-03-10 21:38:01 -0800

    serve availability

--- schedule_lessons/scheduler/urls.py
+++ schedule_lessons/scheduler/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('get_tutors', views.get_tutors, name='get_tutors'),
     path('get_events', views.get_events, name='get_events'),
+    path('availability/<tutor_id>', views.get_availability, name='get_availability')
 ]
