9e822dfdef5fc8a963dc5f95cc799e1e32e057bf
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-03-10 23:10:45 -0800

    add event to database from api post

--- schedule_lessons/scheduler/urls.py
+++ schedule_lessons/scheduler/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('get_tutors', views.get_tutors, name='get_tutors'),
     path('get_events', views.get_events, name='get_events'),
+    path('set_event', views.set_event, name='set_event'),
     path('availability/<tutor_id>', views.get_availability, name='get_availability')
 ]
