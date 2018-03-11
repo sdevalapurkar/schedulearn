d134e132906aabfd329620d9c348b261a102093d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-03-10 18:11:22 -0800

    lessons-1: add schedule-lesson modal

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(function () {
+        $('#startDatePicker').datetimepicker();
+        $('#endDatePicker').datetimepicker();
+    });
+
+    $('#scheduleLessonModal').on('show.bs.modal', function (event) {
+        let saveButton = $(this).find('#saveSchedule');
+        saveButton.click(function () {
+            let scheduleJSON = {};
+            scheduleJSON.lessonName = $('#lessonName').val();
+            scheduleJSON.lessonDescription = $('#lessonDescription').val();
+            scheduleJSON.startDate = $('#startDatePicker').find('input').val();
+            scheduleJSON.endDate = $('#endDatePicker').find('input').val();
+        });
+    });
+});
