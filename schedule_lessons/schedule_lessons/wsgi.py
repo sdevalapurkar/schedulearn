9f0bdff391fa2ccd0a19642ed8b9760df2259c9f
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-03-11 08:37:16 -0700

    ok

--- schedule_lessons/schedule_lessons/wsgi.py
+++ schedule_lessons/schedule_lessons/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schedule_lessons.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schedule_lessons.schedule_lessons.settings") 
 
 application = get_wsgi_application()
