d12545b0bc93f7116867d1198b09204d5e879ede
LeeZeitz <andrew.l.zeitz@gmail.com>
2018-03-11 07:56:33 -0700

    static files yo

--- schedule_lessons/schedule_lessons/settings.py
+++ schedule_lessons/schedule_lessons/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
