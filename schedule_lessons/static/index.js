dac0c82dd939fa0fcfbd5c86dd5a8b59dc5d3d53
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-03-17 00:30:00 -0700

    update my profile page

--- schedule_lessons/static/index.js
+++ schedule_lessons/static/index.js
@@ -175,6 +175,24 @@ function renderHomepage() {
     window.open('/home', "_self");
 }
 
+function previewFile(){
+    var preview = document.querySelector('img'); //selects the query named img
+    var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+    var reader  = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    } else {
+        preview.src = "";
+    }
+}
+
+previewFile();  //calls the function named previewFile()
+
 function sortTable(table, order) {
     console.log(table, order);
     var asc = order === 'asc',
