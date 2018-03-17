0faf7e293846ddecee51d61b67c493022645288d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-03-16 22:49:42 -0700

    add home button to avail page, fix sign up and login to use ahmed's UI design

--- schedule_lessons/signup/forms.py
+++ schedule_lessons/signup/forms.py
@@ -10,7 +10,7 @@ class MyRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('email', 'username', 'first_name', 'last_name', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super(MyRegistrationForm, self).save(commit=False)
