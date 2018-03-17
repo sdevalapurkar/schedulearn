895c15dc99db5f4839e2dd9754cd4faf2806a34d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-03-16 21:14:39 -0700

    fix static dir

--- schedule_lessons/schedule_lessons/settings.py
+++ schedule_lessons/schedule_lessons/settings.py
@@ -16,10 +16,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/login'
