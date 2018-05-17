197129204f82b1317cfd825ca5908500e798f6eb
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-05-17 13:15:20 +0300

    Created some files for heroku deployment

--- schedule_lessons/schedule_lessons/settings.py
+++ schedule_lessons/schedule_lessons/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -138,3 +138,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
