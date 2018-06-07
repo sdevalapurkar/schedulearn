5d2b66b2000f071afefe3d068f8b83fe4c3f139f
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-06-07 23:01:51 +0300

    Remove RegisterationForm and create a manual form

--- schedule_lessons/home/urls.py
+++ schedule_lessons/home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.load_home),
-    path('login/', views.login_view),
-    path('signup/', views.signup_view),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
 ]
