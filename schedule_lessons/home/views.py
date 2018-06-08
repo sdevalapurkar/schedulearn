c763c643302d7877304b24d6325b426cb0e2ccc3
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-06-08 13:55:25 +0300

    Separate sign up for teacher and student

--- schedule_lessons/home/views.py
+++ schedule_lessons/home/views.py
@@ -13,36 +13,72 @@ def signup_view(request):
     if request.method == 'POST':
         #User has send post request with information such as email, password
         #etc and they want to sing up.
-
-        if request.POST.get('password1') == request.POST.get('password2'):
-            # The user enterered the password correctly.
-            username = request.POST.get('username')
-            email = request.POST.get('email')
-            try:
-                user = User.objects.get(username__iexact=username)
-                return render(request, 'index.html', {'username_error': 'Username is already in use',  'email': request.POST.get('email'), 'username': request.POST.get('username'), 'firstName':request.POST.get('firstName'), 'lastName':request.POST.get('lastName')})
-            except User.DoesNotExist:
+        if request.POST.get('teacher_or_student') == 'teacher': #signing up as a tutor
+            if request.POST.get('password1') == request.POST.get('password2'):
+                # The user enterered the password correctly.
+                username = request.POST.get('username')
+                email = request.POST.get('email')
                 try:
-                    user = User.objects.get(email__iexact=email)
-                    return render(request, 'index.html', {'email_error': 'Email is already in use',  'email': request.POST.get('email'), 'username': request.POST.get('username'), 'firstName':request.POST.get('firstName'), 'lastName':request.POST.get('lastName')})
+                    user = User.objects.get(username__iexact=username)
+                    return render(request, 'index.html', {'teacher_username_error': 'Username is already in use',  'teacher_email': request.POST.get('email'), 'teacher_username': request.POST.get('username'), 'teacher_firstName':request.POST.get('firstName'), 'teacher_lastName':request.POST.get('lastName')})
                 except User.DoesNotExist:
-                    # If code gets here it means that email and username are not used and we can begin password validation.
                     try:
-                        validate_password(request.POST.get('password1'))
-                    except ValidationError as password_errors:
-                        return render(request, 'index.html', {'password_errors': password_errors,  'email': request.POST.get('email'), 'username': request.POST.get('username'), 'firstName':request.POST.get('firstName'), 'lastName':request.POST.get('lastName')})
-                    #password validated, ready to put user in database.
+                        user = User.objects.get(email__iexact=email)
+                        return render(request, 'index.html', {'teacher_email_error': 'Email is already in use','teacher_email': request.POST.get('email'), 'teacher_username': request.POST.get('username'), 'teacher_firstName':request.POST.get('firstName'), 'teacher_lastName':request.POST.get('lastName')})
+                    except User.DoesNotExist:
+                        # If code gets here it means that email and username are not used and we can begin password validation.
+                        try:
+                            validate_password(request.POST.get('password1'))
+                        except ValidationError as password_errors:
+                            return render(request, 'index.html', {'teacher_password_errors': password_errors,  'teacher_email': request.POST.get('email'), 'teacher_username': request.POST.get('username'), 'teacher_firstName':request.POST.get('firstName'), 'teacher_lastName':request.POST.get('lastName')})
+                        #password validated, ready to put user in database.
 
-                    user = User.objects.create_user(username, email=request.POST['email'], password=request.POST['password1'])
-                    user.first_name = request.POST['firstName']
-                    user.last_name = request.POST['lastName']
-                    user.profile.profile_pic = 'default/man.png'
-                    user.profile.user_type = request.POST.get('teacher_or_student')
-                    user.save()
-                    login(request, user)
-                    return redirect('dashboard')
-        else:
-            return render(request, 'index.html', {'password_error': 'Passwords do not match.', 'email': request.POST.get('email'), 'username': request.POST.get('username'), 'firstName':request.POST.get('firstName'), 'lastName':request.POST.get('lastName')})
+                        user = User.objects.create_user(username, email=request.POST['email'], password=request.POST['password1'])
+                        user.first_name = request.POST['firstName']
+                        user.last_name = request.POST['lastName']
+                        user.profile.profile_pic = 'default/man.png'
+                        user.profile.user_type = request.POST.get('teacher_or_student')
+                        user.save()
+                        login(request, user)
+                        return redirect('dashboard')
+            else:
+                return render(request, 'index.html', {'teacher_password_error': 'Passwords do not match.', 'teacher_email': request.POST.get('email'), 'teacher_username': request.POST.get('username'), 'teacher_firstName':request.POST.get('firstName'), 'teacher_lastName':request.POST.get('lastName')})
+
+        else: #signing up as a student
+            if request.POST.get('password1') == request.POST.get('password2'):
+                # The user enterered the password correctly.
+                username = request.POST.get('username')
+                email = request.POST.get('email')
+                try:
+                    user = User.objects.get(username__iexact=username)
+                    return render(request, 'index.html', {'student_username_error': 'Username is already in use',  'student_email': request.POST.get('email'), 'username': request.POST.get('username'), 'firstName':request.POST.get('firstName'), 'lastName':request.POST.get('lastName')})
+                except User.DoesNotExist:
+                    try:
+                        user = User.objects.get(email__iexact=email)
+                        return render(request, 'index.html', {'student_email_error': 'Email is already in use',  'email': request.POST.get('email'), 'username': request.POST.get('username'), 'firstName':request.POST.get('firstName'), 'lastName':request.POST.get('lastName')})
+                    except User.DoesNotExist:
+                        # If code gets here it means that email and username are not used and we can begin password validation.
+                        try:
+                            validate_password(request.POST.get('password1'))
+                        except ValidationError as password_errors:
+                            return render(request, 'index.html', {'student_password_errors': password_errors, 'teacher_email': request.POST.get('email'), 'teacher_username': request.POST.get('username'), 'teacher_firstName':request.POST.get('firstName'), 'teacher_lastName':request.POST.get('lastName')})
+                        #password validated, ready to put user in database.
+
+                        user = User.objects.create_user(username, email=request.POST['email'], password=request.POST['password1'])
+                        user.first_name = request.POST['firstName']
+                        user.last_name = request.POST['lastName']
+                        user.profile.profile_pic = 'default/man.png'
+                        user.profile.user_type = request.POST.get('teacher_or_student')
+                        user.save()
+                        login(request, user)
+                        return redirect('dashboard')
+            else:
+                return render(request, 'index.html', {'student_password_error': 'Passwords do not match.', 'teacher_email': request.POST.get('email'), 'teacher_username': request.POST.get('username'), 'teacher_firstName':request.POST.get('firstName'), 'teacher_lastName':request.POST.get('lastName')})
+
+
+
+
+
     else:
         #User want to access homepage.
         return render(request, "index.html")
