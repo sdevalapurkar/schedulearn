23ad56c5ffe72f7b5b76b960ca3920df6ea8b387
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-06-24 20:51:22 +0300

    Remove debugging code

--- schedule_lessons/home/urls.py
+++ schedule_lessons/home/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.load_home),
+    path('', views.load_home, name='home'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('auth/', include('social_django.urls', namespace='social')),
-    path('welcome_user_type/', views.welcome, name='welcome'),
+    path('welcome/', views.welcome, name='welcome'),
 
 ]
