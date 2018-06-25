87d762b111884acb0d2917fd43829d9a1a288953
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-06-26 02:27:16 +0300

    Implemented homepage and started accounts app

---
+++ schedule_lessons/home/static/home/js/homepage.js
@@ -0,0 +1,13 @@
+/* Added a comment*/
+
+$(document).ready(function() {
+
+	$('.learn-more-btn').click(function() {
+
+		var link = $(this).attr('href');
+
+		$('html, body').animate({
+			scrollTop: $(link).offset().top}, 600);
+		return false;
+	});
+});
