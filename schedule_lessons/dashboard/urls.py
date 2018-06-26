7ea1575188eaaa9f6f79ac2a3dc7baa6cf25faf4
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-06-26 23:31:57 +0300

    Clean up adding tutors flow

--- schedule_lessons/dashboard/urls.py
+++ schedule_lessons/dashboard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('get_tutors', views.get_tutors, name='get_tutors'),
     path('get_events', views.get_events, name='get_events'),
     path('set_event', views.set_event, name='set_event'),
@@ -13,7 +13,7 @@ urlpatterns = [
     path('my_profile', views.my_profile, name='my_profile'),
     path('my_profile/edit_profile_pic/', views.edit_profile_pic, name='edit_profile_pic'),
     path('user_type', views.user_type, name='user_type'),
-    path('add_tutor', views.add_tutor, name='add_tutor'),
+    path('scheduler/add_tutor/', views.add_tutor, name='add_tutor'),
     path('confirm_lesson', views.confirm_lesson, name='confirm_lesson'),
     path('decline_lesson', views.decline_lesson, name='decline_lesson'),
 ]
