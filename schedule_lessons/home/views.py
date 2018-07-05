21c0a7d88fc765e68b4f09081adf6c5247043fac
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-07-05 19:36:42 +0300

    If user is already logged in, redirects him to dashboard directly.

--- schedule_lessons/home/views.py
+++ schedule_lessons/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def load_home(request):
+    print(request.user)
+    print(request.user.is_anonymous)
     return render(request, 'home/homepage.html', {request: 'request'})
