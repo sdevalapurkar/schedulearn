5c75bd4b68cf4f38ba9833234adc96867b2e6f6c
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-07-09 15:38:11 +0300

    Remove print statements from views.py in /home/

--- schedule_lessons/home/views.py
+++ schedule_lessons/home/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
 def load_home(request):
-    print(request.user)
-    print(request.user.is_anonymous)
     return render(request, 'home/homepage.html', {request: 'request'})
