6d80ad79029d4554785656edb976016e85adca0f
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-07-17 02:55:07 +0300

    Add TODOs for David to complete as a backend quiz

--- schedule_lessons/schedule_lessons/views.py
+++ schedule_lessons/schedule_lessons/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
-def load_404(request, exception):
-    return render(request, 'errors/404.html')
+# TODO: Create a method that will return the 404 html page with 2 paramters:
+# request and exception. You can return the html page using the render method,
+# view the documentation:
+# https://docs.djangoproject.com/en/2.0/topics/http/shortcuts/#render
+# The html page that you'll need to return is 404.html, and it's in the errors
+# subdirectory (look at the templates folder to figure out hieracy)
