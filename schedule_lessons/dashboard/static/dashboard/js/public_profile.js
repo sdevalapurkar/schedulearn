c4731ea3b199ba1b87bcda441e78179516ef3844
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-07-20 02:08:04 +0300

    Add a border to profile picture and some UI upgrades in the public profile page

--- schedule_lessons/dashboard/static/dashboard/js/public_profile.js
+++ schedule_lessons/dashboard/static/dashboard/js/public_profile.js
@@ -10,22 +10,4 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
-  $("#removeStudentBtn").on({
-    mouseenter: function () {
-      $("#removeStudentBtn").html("<i class='fas fa-user-minus extended-icon'></i>REMOVE STUDENT");
-    },
-    mouseleave: function () {
-      $("#removeStudentBtn").html("<i class='fas fa-user-check extended-icon'></i>STUDENT ADDED");
-    }
-  });
-
-  $("#removeTutorBtn").on({
-    mouseenter: function () {
-      $("#removeTutorBtn").html("<i class='fas fa-user-minus extended-icon'></i>REMOVE TUTOR");
-    },
-    mouseleave: function () {
-      $("#removeTutorBtn").html("<i class='fas fa-user-check extended-icon'></i>TUTOR ADDED");
-    }
-  });
-
 });
