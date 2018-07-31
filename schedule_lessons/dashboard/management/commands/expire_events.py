7041679fea28b7b2ef79eeb5410cbcf5be5e95d7
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-08-01 01:05:14 +0300

    Set up admin commands to delete expired events, and with cron you can run 'python manage.py expire_events' to delete expired events regularly.

---
+++ schedule_lessons/dashboard/management/commands/expire_events.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from dashboard.models import Lesson
+import datetime
+
+class Command(BaseCommand):
+
+    help = 'Expires event objects which are out-of-date'
+
+    def handle(self, *args, **options):
+        Lesson.objects.filter(end_time__lt=datetime.datetime.now()).delete()
