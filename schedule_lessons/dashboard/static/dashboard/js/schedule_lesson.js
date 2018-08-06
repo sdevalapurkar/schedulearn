b5f606a317eb1da72b9ab5782135ceac244a1dba
Ahmed <jesuisahmedn@gmail.com>
2018-08-06 20:54:20 +0300

    Submit lesson form using ajax (schedule_lesson)
    This commit is similar to the previous few commits. The purpose was to
    submit form using ajax like it was done in edit_availabilty. This helped
    clean a lot of the html code up, and now there's realtime error checking
    and no need for refreshing the page.

--- schedule_lessons/dashboard/static/dashboard/js/schedule_lesson.js
+++ schedule_lessons/dashboard/static/dashboard/js/schedule_lesson.js
@@ -13,11 +13,66 @@ $(document).ready(function() {
   });
 
   $("#addBtn").click(function() {
-    $("#submit").click();
+    let form_data = $('form#scheduleForm').serialize() + "&timezoneInfo=" + (new Date().getTimezoneOffset() * -1);
+    $.ajax({
+      url: window.location.href,
+      type: 'post',
+      data: form_data,
+      error: function(xhr, status) {
+        $('.error-list').html('Something went wrong, please make sure your information is correct and try again.');
+        $('#titleLabel').addClass('label-error');
+        $('#titleInput').addClass('input-error');
+        $('#locationLabel').addClass('label-error');
+        $('#locationInput').addClass('input-error');
+        $('#dateLabel').addClass('label-error');
+        $('#dateInput').addClass('input-error');
+        $('#startTimeLabel').addClass('label-error');
+        $('#startTimeInput').addClass('input-error');
+        $('#endTimeLabel').addClass('label-error');
+        $('#endTimeInput').addClass('input-error');
+      },
+      success: function(data) {
+        if (data['status'] == 200) {
+          $('#scheduleVerification').html(data['schedule_success']);
+        } else {
+          if (data['name_error']) {
+            $('#titleLabel').addClass('label-error');
+            $('#titleInput').addClass('input-error');
+          } else {
+            $('#titleLabel').removeClass('label-error');
+            $('#titleInput').removeClass('input-error');
+          }
+          if (data['location_error']) {
+            $('#locationLabel').addClass('label-error');
+            $('#locationInput').addClass('input-error');
+          } else {
+            $('#locationLabel').removeClass('label-error');
+            $('#locationInput').removeClass('input-error');
+          }
+          if (data['date_error']) {
+            $('#dateLabel').addClass('label-error');
+            $('#dateInput').addClass('input-error');
+          } else {
+            $('#dateLabel').removeClass('label-error');
+            $('#dateInput').removeClass('input-error');
+          }
+          if (data['starting_time_error']) {
+            $('#startTimeLabel').addClass('label-error');
+            $('#startTimeInput').addClass('input-error');
+          } else {
+            $('#startTimeLabel').removeClass('label-error');
+            $('#startTimeInput').removeClass('input-error');
+          }
+          if (data['ending_time_error']) {
+            $('#endTimeLabel').addClass('label-error');
+            $('#endTimeInput').addClass('input-error');
+          } else {
+            $('#endTimeLabel').removeClass('label-error');
+            $('#endTimeInput').removeClass('input-error');
+          }
+        }
+      },
+    });
   });
-
-  var offset = new Date().getTimezoneOffset();
-  offset *= (-1)
-  $('#timeZoneInput').attr('value', offset);
 
 });
