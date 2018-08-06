158ca7c3a6fc2e0a34841029a0bed31d1562de68
Ahmed <jesuisahmedn@gmail.com>
2018-08-06 19:07:59 +0300

    Submit availability form using ajax (edit_availability)
    Purpose of this commit was to stop submitting the availability form
    using html and do so using ajax. The advantage of this is that the
    website doesn't need to refresh if there is an error in the input
    form and this way keeps the html code cleaner. Eventually, we'd want
    to remove the availability table code from the html to the js too,
    but for this commit, I'll leave it at availability form.

--- schedule_lessons/dashboard/static/dashboard/js/edit_availability.js
+++ schedule_lessons/dashboard/static/dashboard/js/edit_availability.js
@@ -9,7 +9,36 @@ $(document).ready(function() {
   });
 
   $("#addBtn").click(function() {
-    $("#submit").click();
+    $.ajax({
+      url: '/dashboard/my_profile/edit_availability/',
+      type: 'post',
+      data: $('form#availabilityForm').serialize(),
+      error: function(xhr, status) {
+        $('.error-list').html('Something went wrong, please refresh and try again.');
+      },
+      success: function(data) {
+        if (data['status'] == 200) {
+          location.reload();
+        } else {
+          if (data['time_error']) {
+            $('.error-list').html(data['time_error']);
+            $('#startTimeLabel').addClass('label-error');
+            $('#startTimeInput').addClass('input-error');
+            $('#endTimeLabel').addClass('label-error');
+            $('#endTimeInput').addClass('input-error');
+          } else {
+            if (data['starting_time_error']) {
+              $('#startTimeLabel').addClass('label-error');
+              $('#startTimeInput').addClass('input-error');
+            }
+            if (data['ending_time_error']) {
+              $('#endTimeLabel').addClass('label-error');
+              $('#endTimeInput').addClass('input-error');
+            }
+          }
+        }
+      },
+    });
   });
 
   var offset = new Date().getTimezoneOffset();
