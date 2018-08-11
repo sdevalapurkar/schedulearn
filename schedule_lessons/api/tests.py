96459f85e46fc8f5c110c35a7aa90998ad9c7f12
Ahmed <jesuisahmedn@gmail.com>
2018-08-11 16:42:39 +0300

    Add RESTful API access via DRF
    Pip installed djangorestframework and created a django app called api
    with views setup to view existing users in schedulearn. Commit was
    necessary to setup boiler-plate for further access to schedulearn db

---
+++ schedule_lessons/api/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+from dashboard.models import *
+from accounts.models import *
+
+# Create your tests here.
---
+++ schedule_lessons/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import routers, serializers, viewsets
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
