4bb9c63ab86c15c7f0868a120cc3884d6162501b
Ahmed <jesuisahmedn@gmail.com>
2018-08-13 00:24:56 +0300

    Add api route to get user info

--- schedule_lessons/api/urls.py
+++ schedule_lessons/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 # from api.views import UserViewSet
+from .views import get_user
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
+    path('get_user/', get_user)
 ]
