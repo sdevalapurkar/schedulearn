75fade09ca6fd31542246efac098481a331f77a9
Ahmed <jesuisahmedn@gmail.com>
2018-08-14 02:11:28 +0300

    Add api endpoint to return connections of a user via a token

--- schedule_lessons/api/serializers.py
+++ schedule_lessons/api/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from accounts.models import Profile
+from dashboard.models import Relationship
+from django.contrib.auth.models import User
+
 
 # Serializers define the API representation.
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+
+# Serializer for Relationships
+class ConnectionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Relationship
+        fields = ('tutor','student')
