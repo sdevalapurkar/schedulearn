04afa89e11207f0114862c44eeec5bee692cd8ab
Ahmed <jesuisahmedn@gmail.com>
2018-08-14 02:11:28 +0300

    Add api endpoint to return connections of a user via a token

--- schedule_lessons/api/urls.py
+++ schedule_lessons/api/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
-    path('profile/<token>', Profile.as_view())
+    path('profile/<token>', Profile.as_view()),
+    path('connections/<token>', Connections.as_view())
 ]
--- schedule_lessons/api/views.py
+++ schedule_lessons/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
-from .serializers import ProfileSerializer
+from .serializers import *
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.http import Http404
+from dashboard.models import Relationship
 
 class Profile(APIView):
     '''
@@ -20,3 +21,35 @@ class Profile(APIView):
         profile = self.get_profile(token)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+class Connections(APIView):
+    '''
+    GET REQUEST: Returns all connections given a token.
+    POST REQUEST: Add a connection given a token.
+    '''
+    def get_user(self, token):
+        try:
+            token = Token.objects.get(key=token)
+            return token.user
+        except Token.DoesNotExist:
+            raise Http404
+
+    def get(self, request, token):
+        user = self.get_user(token)
+        connections = []
+        relationships = Relationship.objects.filter(tutor=user) if user.profile.user_type == 'tutor' else Relationship.objects.filter(student=user)
+        for relationship in relationships:
+            if user.profile.user_type == 'tutor':
+                profile_serializer = ProfileSerializer(relationship.student.profile)
+                person_info = profile_serializer.data
+                person_info['first_name'] = relationship.student.first_name
+                person_info['last_name'] = relationship.student.last_name
+            else:
+                profile_serializer = ProfileSerializer(relationship.tutor.profile)
+                person_info = profile_serializer.data
+                person_info['first_name'] = relationship.tutor.first_name
+                person_info['last_name'] = relationship.tutor.last_name
+
+            connections.append(person_info)
+
+        return Response({'connections': connections})
