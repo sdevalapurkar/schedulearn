3d2391c87a5cd7c64aedb7940bf8721379838498
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-11-09 15:32:47 -0800

    Change all unread notifications to read when someone checks on their notification via an AJAX request

--- schedule_lessons/dashboard/static/dashboard/js/agenda.js
+++ schedule_lessons/dashboard/static/dashboard/js/agenda.js
@@ -8,4 +8,44 @@ $(document).ready(function() {
     $('#declineLessonModal').on('show.bs.modal', function (e) {
       document.getElementById('declineLessonConfirm').setAttribute("href", e.relatedTarget.href);
     });
+
+    $('#notifcationsDropdownLink').click(function() {
+      if (document.getElementById("notificationIcon").hasAttribute("data-count")) {
+        $('#notificationIcon').removeAttr("data-count");
+        $('#notificationIcon').removeClass("notification-badge");
+      }
+      $.ajax({
+        url: '/dashboard/clear_notifications/',
+        type: 'post',
+        error: function(xhr, status) {
+        },
+        success: function(data) {
+        }
+      });
+    });
+});
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+$.ajaxSetup({
+     beforeSend: function(xhr, settings) {
+         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+             // Only send the token to relative URLs i.e. locally.
+             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+         }
+     }
 });
