f07731a81023a96c1b41e8a3d5077077899231bb
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-11-09 15:32:47 -0800

    Change all unread notifications to read when someone checks on their notification via an AJAX request

--- schedule_lessons/dashboard/static/dashboard/js/my_profile.js
+++ schedule_lessons/dashboard/static/dashboard/js/my_profile.js
@@ -130,6 +130,21 @@ $(document).ready(function() {
       },
     })
   }); // click handler END for #confirmChangePassword
+
+  $('#notifcationsDropdownLink').click(function() {
+    if (document.getElementById("notificationIcon").hasAttribute("data-count")) {
+      $('#notificationIcon').removeAttr("data-count");
+      $('#notificationIcon').removeClass("notification-badge");
+    }
+    $.ajax({
+      url: '/dashboard/clear_notifications/',
+      type: 'post',
+      error: function(xhr, status) {
+      },
+      success: function(data) {
+      }
+    });
+  });
 }); // Document.ready function END
 
 
--- schedule_lessons/dashboard/static/dashboard/js/relationships.js
+++ schedule_lessons/dashboard/static/dashboard/js/relationships.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('#notifcationsDropdownLink').click(function() {
+      if (document.getElementById("notificationIcon").hasAttribute("data-count")) {
+        $('#notificationIcon').removeAttr("data-count");
+        $('#notificationIcon').removeClass("notification-badge");
+      }
+      $.ajax({
+        url: '/dashboard/clear_notifications/',
+        type: 'post',
+        error: function(xhr, status) {
+        },
+        success: function(data) {
+        }
+      });
+    });
 });
