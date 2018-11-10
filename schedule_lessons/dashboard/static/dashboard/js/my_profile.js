ca287341a792969e6941f65ae43cc602dcead503
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-11-10 14:59:28 -0800

    Fixes issue 69 and improves indentation

--- schedule_lessons/dashboard/static/dashboard/js/my_profile.js
+++ schedule_lessons/dashboard/static/dashboard/js/my_profile.js
@@ -5,18 +5,7 @@ $(document).ready(function() {
   $("#reviewTab").fadeOut(0);
   $("#historyTab").fadeOut(0);
 
-  $('#deleteAccountConfirm').click(function() {
-    $('#deleteAccountModal').modal('hide');
-    $.ajax({
-        type: 'DELETE',
-        url: '/dashboard/my_profile/delete_account/',
-        success: function () {
-          document.location.href = "/";
-        }
-    });
-  });
-
-  $("ul li").click(function(e) {
+  $(".slider-line .s-line-options").click(function(e) {
 
     // make sure we cannot click the slider
     if ($(this).hasClass('slider')) {
@@ -29,8 +18,7 @@ $(document).ready(function() {
     var clickedTab = $(this).index();
 
     // Work out how far the slider needs to go
-    var sliderMovesBy = 120
-    * clickedTab;
+    var sliderMovesBy = 120 * clickedTab;
 
     $(".slider").css({
       left: sliderMovesBy + "px"
@@ -101,6 +89,19 @@ $(document).ready(function() {
     $("#reviewTab").fadeOut(300);
   });
 
+  // END script from https://www.jqueryscript.net/menu/Material-Design-Sliding-Tab-Menu-With-jQuery-CSS3.html
+
+  $('#deleteAccountConfirm').click(function() {
+    $('#deleteAccountModal').modal('hide');
+    $.ajax({
+      type: 'DELETE',
+      url: '/dashboard/my_profile/delete_account/',
+      success: function () {
+        document.location.href = "/";
+      }
+    });
+  });
+
   $("#confirmChangePassword").on("click", function() {
     $.ajax({
       type: 'POST',
@@ -150,26 +151,26 @@ $(document).ready(function() {
 
 
 function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-            // Only send the token to relative URLs i.e. locally.
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
+  beforeSend: function(xhr, settings) {
+    if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+      // Only send the token to relative URLs i.e. locally.
+      xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
     }
+  }
 });
