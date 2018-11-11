60e30312a3ccd38dea5186a8353d8225a24ca915
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-11-11 13:02:52 -0800

    Clean the django code in home app using pylint

--- schedule_lessons/home/views.py
+++ schedule_lessons/home/views.py
@@ -1,4 +1,6 @@
+'''This module contains the view that renders the homepage.'''
 from django.shortcuts import render
 
 def load_home(request):
+    '''This view serves the homepage to the user.'''
     return render(request, 'home/homepage.html', {request: 'request'})
