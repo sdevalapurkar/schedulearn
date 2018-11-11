686a83598019c5173d8d009c4e937bdf7d8e7d26
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-11-11 01:16:12 -0800

    Clean python code in dashboard app

--- schedule_lessons/dashboard/management/commands/expire_events.py
+++ schedule_lessons/dashboard/management/commands/expire_events.py
@@ -1,10 +1,12 @@
+'''This module defines commands for crontab to execute regularly.'''
+import datetime
 from django.core.management.base import BaseCommand
 from dashboard.models import Lesson
-import datetime
 
 class Command(BaseCommand):
-
+    '''This class defines commands for crontab to execute regularly.'''
     help = 'Expires event objects which are out-of-date'
-
     def handle(self, *args, **options):
-        Lesson.objects.filter(end_time__lt=datetime.datetime.now(datetime.timezone.utc)).delete()
+        '''This method describes the actions to perform for crontab.'''
+        Lesson.objects.filter(end_time__lt=datetime.datetime.now(
+            datetime.timezone.utc)).delete()
