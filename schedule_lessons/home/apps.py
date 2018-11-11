4efabd65e1ed12edaca8f9b3d217df70b42db4dd
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2018-11-11 13:02:52 -0800

    Clean the django code in home app using pylint

--- schedule_lessons/home/apps.py
+++ schedule_lessons/home/apps.py
@@ -1,5 +1,7 @@
+'''This module configures the home package as a django app.'''
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    '''This class configures the name of the home app.'''
     name = 'home'
