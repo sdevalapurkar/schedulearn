c8a074a6a1ca4d8d5d086c836d5ff43a0791cd32
Nisarg Patel <np96380@gmail.com>
2018-12-11 16:46:05 +0530

    REmoved unncessary whitespace and cleaned up code

--- schedule_lessons/dashboard/static/dashboard/js/agenda.js
+++ schedule_lessons/dashboard/static/dashboard/js/agenda.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="tooltip"]').tooltip();
 
-    $(".option-icon").click(function(){
-        $("#twoButtons").toggleClass("buttons-showHide");
-        $(".option-icon").toggleClass("slide-position");
-      });
+  $(".option-icon").click(function(){
+  $("#twoButtons").toggleClass("buttons-showHide");
+    $(".option-icon").toggleClass("slide-position");
+  });
 });
--- schedule_lessons/dashboard/static/dashboard/js/edit_profile.js
+++ schedule_lessons/dashboard/static/dashboard/js/edit_profile.js
@@ -1,32 +1,29 @@
 var img_url;
 
 $(document).ready(function () {
-
   $("#saveChanges").click(function() {
     $("#profileSave").click();
   });
 
   /* ALL CODE BELOW RELATED TO CROPPING PICTURE */
-
   var cropped = $("#cropPicture").croppie({
     viewport: {
-        width: 200,
-        height: 200,
-        type: 'square'
+      width: 200,
+      height: 200,
+      type: 'square'
     }
   });
 
   $("#inputPfp").change(function () {
     $("#cropPictureModal").modal();
     if (this.files && this.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $('#cropPicture').attr('src', e.target.result);
-            img_url = e.target.result;
-        }
-        reader.readAsDataURL(this.files[0]);
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#cropPicture').attr('src', e.target.result);
+        img_url = e.target.result;
+      }
+      reader.readAsDataURL(this.files[0]);
     }
-
   });
 
   $('#cropPictureModal').on('shown.bs.modal', function (e) {
@@ -38,50 +35,47 @@ $(document).ready(function () {
 
   $("#saveCrop").on("click", function(ev) {
     cropped.croppie('result', {
-								type: 'base64',
-								format: 'jpeg',
-								size: {width: 200, height: 200}
-							}).then(function (resp) {
-                $.ajax({
-                    type: 'POST',
-                    url: '/dashboard/my_profile/edit_profile/',
-                    data: {'profile_pic': resp},
-                    success: function () {
-                        location.reload();
-                    }
-                });
-
-								$('#cropPictureModal').modal('hide');
-              })
+	  type: 'base64',
+	  format: 'jpeg',
+	  size: {width: 200, height: 200}
+	}).then(function (resp) {
+      $.ajax({
+        type: 'POST',
+        url: '/dashboard/my_profile/edit_profile/',
+        data: {'profile_pic': resp},
+        success: function () {location.reload();}
+      });
+    
+      $('#cropPictureModal').modal('hide');
+    });
   });
 
   $(".text").on("click", function() {
     $("#inputPfp").click();
   });
-
 });
 
 function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
 $.ajaxSetup({
-     beforeSend: function(xhr, settings) {
-         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-             // Only send the token to relative URLs i.e. locally.
-             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-         }
-     }
+  beforeSend: function(xhr, settings) {
+    if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+      // Only send the token to relative URLs i.e. locally.
+      xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }
+  }
 });
