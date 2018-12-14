5c25bf5392f9385d0cf6ff46df5a76b9c9753817
Nisarg Patel <np96380@gmail.com>
2018-12-15 01:02:26 +0530

    mobile friendly redesign and cleaner properly indented code

--- schedule_lessons/dashboard/static/dashboard/js/edit_availability.js
+++ schedule_lessons/dashboard/static/dashboard/js/edit_availability.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   $('#startingTime').datetimepicker({
     format: 'LT'
@@ -8,20 +8,20 @@ $(document).ready(function() {
     format: 'LT'
   });
 
-  $("#addBtn").click(function() {
+  $("#addBtn").click(function () {
     let form_data = $('form#availabilityForm').serialize() + "&timezoneInfo=" + (new Date().getTimezoneOffset() * -1);
     $.ajax({
       url: '/dashboard/my_profile/edit_availability/',
       type: 'post',
       data: form_data,
-      error: function(xhr, status) {
+      error: function (xhr, status) {
         $('.error-list').html('Something went wrong, please make sure your information is correct and try again.');
         $('#startTimeLabel').addClass('label-error');
         $('#startTimeInput').addClass('input-error');
         $('#endTimeLabel').addClass('label-error');
         $('#endTimeInput').addClass('input-error');
       },
-      success: function(data) {
+      success: function (data) {
         if (data['status'] == 200) {
           location.reload();
         } else {
@@ -46,7 +46,7 @@ $(document).ready(function() {
     });
   });
 
-  $('#notificationsDropdownLink').click(function() {
+  $('#notificationsDropdownLink').click(function () {
     var attr = $('#notificationIcon').attr('data-count');
     if (typeof attr !== typeof undefined && attr !== false) {
       $('#notificationIcon').removeAttr("data-count");
@@ -55,35 +55,35 @@ $(document).ready(function() {
     $.ajax({
       url: '/dashboard/clear_notifications/',
       type: 'post',
-      error: function(xhr, status) {
+      error: function (xhr, status) {
       },
-      success: function(data) {
+      success: function (data) {
       }
     });
   });
 });
 
 function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
 $.ajaxSetup({
-     beforeSend: function(xhr, settings) {
-         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-             // Only send the token to relative URLs i.e. locally.
-             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-         }
-     }
+  beforeSend: function (xhr, settings) {
+    if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+      // Only send the token to relative URLs i.e. locally.
+      xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }
+  }
 });
