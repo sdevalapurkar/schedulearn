a7a8ca8d858318bf389d532ed523209d51f3d829
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-01-02 10:57:23 -0800

    hotfix

--- schedule_lessons/local_settings.py
+++
@@ -1,5 +0,0 @@
-EMAIL_HOST = 'smtp.zoho.com'
-VERIFY_USER_EMAIL = 'verify.user@schedulearn.com'
-FORGET_PASSWORD_EMAIL = 'reset.password@schedulearn.com'
-EMAIL_HOST_PASSWORD = 'Schedulearn2018'
-EMAIL_PORT = 587
