e57764c2f57eb55781102f5ae3815a3863815369
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2019-01-02 17:27:32 -0800

    Remove username and add names fields when registering a user through the Schedulearn API

--- schedule_lessons/schedule_lessons/local_settings.py
+++ schedule_lessons/schedule_lessons/local_settings.py
@@ -3,3 +3,4 @@ VERIFY_USER_EMAIL = 'verify.user@schedulearn.com'
 FORGET_PASSWORD_EMAIL = 'reset.password@schedulearn.com'
 EMAIL_HOST_PASSWORD = 'Schedulearn2018'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
