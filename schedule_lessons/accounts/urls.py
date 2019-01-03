7408afbd82992aa6f9d5d373ca67bec2684bb1a5
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2019-01-03 11:18:24 -0800

    Use the view functions created by allauth to handle signup, login, logout

--- schedule_lessons/accounts/urls.py
+++ schedule_lessons/accounts/urls.py
@@ -1,15 +1,15 @@
 '''This module is used to create routes after /accounts/'''
 from django.urls import path, include
-from django.contrib.auth.views import logout
 from . import views
-
+from allauth.account.views import logout, email, login, signup
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
+    path('signup/', signup, name='account_signup'),
     path('personalize/', views.personalize_view, name='personalize'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', logout, name='logout'),
+    path('login/', login, name='account_login'),
+    path('logout/', logout, name='account_logout'),
+    path('email/', email, name='account_email'),
     path('forget_password', views.forget_password, name='forget_password'),
-    path('reset_password/<user_id>', views.reset_password, name='reset_password'),
+    path('reset_password/', views.reset_password, name='account_reset_password'),
     path('verify_email/<user_id>', views.verify_email, name='verify_email'),
     path('auth/', include('social_django.urls', namespace='social')),
 ]
