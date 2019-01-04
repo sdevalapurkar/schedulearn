bd64ca7c9c561d13bd8961589150df34cf433ccf
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2019-01-03 21:29:05 -0800

    Remove social auth from templates

--- schedule_lessons/schedule_lessons/settings.py
+++ schedule_lessons/schedule_lessons/settings.py
@@ -100,8 +100,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -121,7 +119,6 @@ DATABASES = {
 }
 
 AUTHENTICATION_BACKENDS = (
- 'accounts.google_calendar.GoogleCalendar',
  'django.contrib.auth.backends.ModelBackend',
  "allauth.account.auth_backends.AuthenticationBackend"
 )
