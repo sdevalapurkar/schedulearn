29d020655a22989b25278d194b3c4a58ec3421d6
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2019-01-03 22:25:07 -0800

    Create a custom form to handle assigning a name to user

---
+++ schedule_lessons/accounts/forms.py
@@ -0,0 +1,19 @@
+from allauth.account.forms import SignupForm
+from allauth.account.adapter import get_adapter
+from allauth.account.utils import setup_user_email
+
+class MyCustomSignupForm(SignupForm):
+
+    def save(self, request):
+        full_name = request.POST.get('user_name').split()
+        adapter = get_adapter(request)
+        user = adapter.new_user(request)
+        user.first_name = full_name[0]
+        if len(full_name) > 1:
+            user.last_name = full_name[1]
+        user.save()
+        adapter.save_user(request, user, self)
+        self.custom_signup(request, user)
+        # TODO: Move into adapter `save_user` ?
+        setup_user_email(request, user, [])
+        return user
