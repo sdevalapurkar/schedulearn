d4499c4b0bfdb647e7fb8bf09dde6aba1dd3df8f
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2019-01-04 00:23:08 -0800

    Create routing for google logins

--- schedule_lessons/accounts/urls.py
+++ schedule_lessons/accounts/urls.py
@@ -4,7 +4,8 @@ from . import views
 from allauth.account.views import (logout, email, login, signup, password_reset,
                                    password_reset_done, password_reset_from_key,
                                    password_reset_from_key_done)
-
+from allauth.socialaccount import providers
+from importlib import import_module
 
 urlpatterns = [
     path('signup/', signup, name='account_signup'),
@@ -17,3 +18,12 @@ urlpatterns = [
     path('reset_password_key/<uidb36>/<key>/', password_reset_from_key, name='account_reset_password_from_key'),
     path('reset_password_key/done', password_reset_from_key_done, name='account_reset_password_from_key_done'),
 ]
+
+for provider in providers.registry.get_list():
+    try:
+        prov_mod = import_module(provider.get_package() + '.urls')
+    except ImportError:
+        continue
+    prov_urlpatterns = getattr(prov_mod, 'urlpatterns', None)
+    if prov_urlpatterns:
+        urlpatterns += prov_urlpatterns
