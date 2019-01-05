08c8d23af7c01f445b959fb0966be9251e078f07
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2019-01-04 17:22:41 -0800

    Change name of variable: full_name to full_name_list

--- schedule_lessons/accounts/forms.py
+++ schedule_lessons/accounts/forms.py
@@ -5,12 +5,12 @@ from allauth.account.utils import setup_user_email
 class MyCustomSignupForm(SignupForm):
 
     def save(self, request):
-        full_name = request.POST.get('user_name').split()
+        full_name_list = request.POST.get('user_name').split()
         adapter = get_adapter(request)
         user = adapter.new_user(request)
-        user.first_name = full_name[0]
-        if len(full_name) > 1:
-            user.last_name = full_name[1]
+        user.first_name = full_name_list[0]
+        if len(full_name_list) > 1:
+            user.last_name = full_name_list[1]
         user.save()
         adapter.save_user(request, user, self)
         self.custom_signup(request, user)
