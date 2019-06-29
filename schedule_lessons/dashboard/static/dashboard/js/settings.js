700f3f641bd50506933599e5933bbbbf5713d52b
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2019-06-29 10:40:06 -0400

    Remove unused css/js code

--- schedule_lessons/dashboard/static/dashboard/js/settings.js
+++ schedule_lessons/dashboard/static/dashboard/js/settings.js
@@ -1,8 +1,4 @@
 $(document).ready(() => {
-  $('#options_btn').click(() => {
-    $('.two-buttons').fadeToggle(300).toggleClass('addAnim');
-    $('#options_btn').toggleClass('addBtnAnim');
-  });
 
   $('#secondNavBtn').click(() => {
     $('#navbarSupportedContent').slideToggle('fast');
