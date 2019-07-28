8c29353cf3983e56ea2d874e96ce0888a2e5d398
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2019-07-28 17:29:24 -0400

    Move media folder to static folder at the root of django project

--- schedule_lessons/schedule_lessons/settings.py
+++ schedule_lessons/schedule_lessons/settings.py
@@ -116,7 +116,7 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/media/')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # ie if Heroku server
