4c573e4a76b653b67a2f531cdfc9e4c486934f03
Nisarg Patel <np96380@gmail.com>
2019-10-13 19:50:48 -0400

    experiment: agenda converted to es6

--- schedule_lessons/dashboard/static/dashboard/js/agenda.js
+++ schedule_lessons/dashboard/static/dashboard/js/agenda.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(() => {
   $('[data-toggle="tooltip"]').tooltip();
 
-  $('.acknowledge-btn').on('click', function(e) {
+  $('.acknowledge-btn').on('click', e => {
     let data = {
       show_tutorial: false
     };
@@ -13,37 +13,33 @@ $(document).ready(function() {
       url: '/dashboard/save_tutorial_preferences/',
       type: 'post',
       data: data,
-      error: function(xhr, status) {
+      error: (xhr, status) => {
         alert('Something went wrong, please try again.');
       }
     });
   });
 
-  $('#cancelLessonModal').on('show.bs.modal', function(e) {
-    document
-      .getElementById('cancelLessonConfirm')
-      .setAttribute('href', e.relatedTarget.href);
+  $('#cancelLessonModal').on('show.bs.modal', e => {
+    document.getElementById('cancelLessonConfirm').setAttribute('href', e.relatedTarget.href);
   });
 
-  $('#options_btn').click(function() {
+  $('#options_btn').click(() => {
     $('.two-buttons')
       .fadeToggle(300)
       .toggleClass('addAnim');
     $('#options_btn').toggleClass('addBtnAnim');
   });
 
-  $('#secondNavBtn').click(function() {
+  $('#secondNavBtn').click(() => {
     $('#navbarSupportedContent').slideToggle('fast');
   });
 
-  $('#declineLessonModal').on('show.bs.modal', function(e) {
-    document
-      .getElementById('declineLessonConfirm')
-      .setAttribute('href', e.relatedTarget.href);
+  $('#declineLessonModal').on('show.bs.modal', e => {
+    document.getElementById('declineLessonConfirm').setAttribute('href', e.relatedTarget.href);
   });
 
-  $('#notificationsDropdownLink').click(function() {
-    var attr = $('#notificationIcon').attr('data-count');
+  $('#notificationsDropdownLink').click(() => {
+    const attr = $('#notificationIcon').attr('data-count');
     if (typeof attr !== typeof undefined && attr !== false) {
       $('#notificationIcon').removeAttr('data-count');
       $('#notificationIcon').removeClass('notification-badge');
@@ -51,18 +47,18 @@ $(document).ready(function() {
     $.ajax({
       url: '/dashboard/clear_notifications/',
       type: 'post',
-      error: function(xhr, status) {},
-      success: function(data) {}
+      error: (xhr, status) => {},
+      success: data => {}
     });
   });
 });
 
-function getCookie(name) {
-  var cookieValue = null;
+const getCookie = name => {
+  let cookieValue = null;
   if (document.cookie && document.cookie != '') {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = jQuery.trim(cookies[i]);
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = jQuery.trim(cookies[i]);
       // Does this cookie string begin with the name we want?
       if (cookie.substring(0, name.length + 1) == name + '=') {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
@@ -71,10 +67,10 @@ function getCookie(name) {
     }
   }
   return cookieValue;
-}
+};
 
 $.ajaxSetup({
-  beforeSend: function(xhr, settings) {
+  beforeSend: (xhr, settings) => {
     if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
       // Only send the token to relative URLs i.e. locally.
       xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
